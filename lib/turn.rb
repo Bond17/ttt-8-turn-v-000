e7fe4da0d193a32b70ea2fce58abd467199afb8b
Robert Watson <spartan83@gmail.com>
2017-12-02 21:24:06 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,16 @@
+def turn
+board = [" "," "," "," "," "," "," "," "," "]
+puts "Please enter 1-9: "
+input = gets.strip
+index = input_to_index(input)
+if valid_move?(board, index)
+display_board(board)
+end
+end
+
+
+
+
 def valid_move?(board,index)
 if index.between?(0,8) && !position_taken?(board,index)
   true
